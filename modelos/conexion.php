1c6ff346f62c6c2df4811e11e39b43880c12bf2b
Benjamin Contreras <91103275+Benjax14@users.noreply.github.com>
2022-05-21 17:42:36 -0400

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host="basedatos:3306"; 
-$db_user="root";
-$db_pass="uwu";
-$db_name="inventariobd";
+$db_host="mysqltrans.face.ubiobio.cl"; 
+$db_user="G55taller";
+$db_pass="G55taller1117";
+$db_name="G55taller_bd";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
