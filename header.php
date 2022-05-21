4dbd62cb7e551f40b8a665e5c436e30e53f9e438
Benjamin Contreras <benjamin.contreras1901@alumnos.ubiobio.cl>
2022-05-21 17:25:06 -0400

    Cambios de css y rutas

--- header.php
+++ header.php
@@ -6,12 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
-   <style type="text/css">
-        <?php
-            $css = file_get_contents('css/style2.css');
-            echo $css;
-        ?>
-    </style>
+    <link rel="stylesheet" href="./css/imagen.css">
 </head>
 <body>
     <header>
--- modelos/editarBackend.php
+++ modelos/editarBackend.php
@@ -6,25 +6,25 @@
         $id = mysqli_real_escape_string ($con, $_POST["clave"]);
         $marca_produc = mysqli_real_escape_string ($con, $_POST["marca_produc"]);
         $nombre_produc = mysqli_real_escape_string ($con, $_POST["nombre_produc"]);
-        $talla = $_POST["select_talla"];
-        //$imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
-        $precio = mysqli_real_escape_string ($con, $_POST["precio_pr"]);
+        $talla = mysqli_real_escape_string ($con, $_POST["select_talla"]);
+        $precio = mysqli_real_escape_string ($con, $_POST["precio"]);
         $stock = mysqli_real_escape_string ($con, $_POST["stock"]);
-        $categoria = $_POST["select_cat"];
-        $estado = $_POST["select"];
+        $categoria = mysqli_real_escape_string ($con, $_POST["select_cat"]);
+        $estado = mysqli_real_escape_string ($con, $_POST["select"]);
+
         $consultaEditar = "UPDATE producto set marca='".$marca_produc."', nombre='".$nombre_produc."', id_estado='".$estado."', precio='".$precio."', stock='".$stock."', id_talla='".$talla."', id_cat='".$categoria."' WHERE id='".$id."'";
-        $aux = mysqli_query($con, $consultaEditar);
-        //echo $consultaEditar;
+        mysqli_query($con, $consultaEditar);
+
         unset($_POST["marca_produc"]);
         unset($_POST["nombre_produc"]);
-        unset($_POST["stock"]);
-        unset($_POST["precio_pr"]);
         unset($_POST["select_talla"]);
+        unset($_POST["precio"]);
+        unset($_POST["stock"]);
         unset($_POST["select_cat"]);
         unset($_POST["select"]);    
 
         header("Location:../index.php");
-        
+         
     }
     
 ?>
