9797d40023f41b47632ecf731c0654794947e48a
Benjamin Contreras <benja.contreras732@gmail.com>
2022-05-21 22:23:28 -0400

    Fix css y limpieza de codigo

--- enlistarTrajes.php
+++ enlistarTrajes.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Admin</title>
-    
-    <script
-		src="https://code.jquery.com/jquery-3.6.0.js"
-		integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-		crossorigin="anonymous">       
-    </script>
+<body> 
 
     <script type="text/javascript">
         function confirmDelete(){
@@ -26,17 +11,8 @@
         } 
     }
     </script>
-    <style>
-    .mostrarnt{
-        display: none;
-    }
-    </style>
-        
-</head>
 
-<body>      
-
-    <?php include('./modelos/enlistarTrajesBackend.php')?>
+    <?php include('./modelos/enlistarTrajesBackend.php');?>
 
     <div class="row justify-content-center">
             <div class="col-auto" style="margin-top: 15px"> 
