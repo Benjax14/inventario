23c7779932b9ca2e61cefd972aea6340dfbc619d
Benjamin Contreras <benja.contreras732@gmail.com>
2022-05-24 01:22:36 -0400

    Comentar funcionalidad

--- modelos/eliminarProductoBackend.php
+++ modelos/eliminarProductoBackend.php
@@ -1,33 +1,12 @@
 <?php
 
-    include_once("conexion.php");
-
-    $ConsultaID = "SELECT * FROM se_puede";
-    $ConsultaIDP = mysqli_query($con, $ConsultaID);
-    
-?>
-
-<?php
-
     include("conexion.php");
 
     $id = $_POST['Prod'];
 
-    foreach($ConsultaIDP as $row):
-    $id_traje = $row['id']; 
-
-    if($id == $id_traje){
-
-        echo '<script language="javascript">alert("Producto Reservado, no es posible eliminar");window.location.href="../index.php"</script>';
-
-    }else{
-
     $sql = "DELETE FROM producto WHERE id = '".$id."'";
     mysqli_query($con, $sql);
-    echo '<script language="javascript">alert("Producto Editado");window.location.href="../index.php"</script>';
+    echo '<script language="javascript">alert("Producto Eliminado");window.location.href="../index.php"</script>';
 
-    }
 
-    endforeach;
-    
 ?>
