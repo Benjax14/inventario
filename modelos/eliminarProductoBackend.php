dff8d5de4db7ac63e706c50506fb2b4375333207
SerapioUnlucky <104412736+SerapioUnlucky@users.noreply.github.com>
2022-05-24 02:13:24 -0400

    prueba

--- modelos/eliminarProductoBackend.php
+++ modelos/eliminarProductoBackend.php
@@ -1,12 +1,35 @@
 <?php
 
-    include("conexion.php");
+    require_once("conexion.php");
+
+    $consultaPuede = "SELECT * FROM se_puede";
+    $consultaPue = mysqli_query($con, $consultaPuede);
+
+?>
+
+<?php
+
+    require_once("conexion.php");
 
     $id = $_POST['Prod'];
 
-    $sql = "DELETE FROM producto WHERE id = '".$id."'";
-    mysqli_query($con, $sql);
-    echo '<script language="javascript">alert("Producto Eliminado");window.location.href="../index.php"</script>';
+    foreach($consultaPue as $row):
 
+    $id_pro = $row['id'];
+
+    if($id == $id_pro){
+        
+        echo '<script language="javascript">alert("Producto se encuentra reservado");window.location.href="../index.php"</script>'; 
+    
+    }else{
+    
+    $eliminar = "DELETE FROM producto WHERE id = '".$id."' ";
+    $eli = mysqli_query($con, $eliminar);
+
+        echo '<script language="javascript">alert("Producto eliminado!");window.location.href="../index.php"</script>';
+        //header("location:../index.php");
+    }
+
+    endforeach
 
 ?>
