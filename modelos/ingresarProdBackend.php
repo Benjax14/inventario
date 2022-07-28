8058537e66de053166b2fc9cbefaf2d44248edfb
Benjamin Contreras <benja.contreras732@gmail.com>
2022-07-28 16:55:47 -0400

    xss

--- modelos/ingresarProdBackend.php
+++ modelos/ingresarProdBackend.php
@@ -1,16 +1,20 @@
 <?php
     include_once("conexion.php");
     
+    if(!empty($_POST) && empty($_FILES['imagen']['tmp_name'])){
+        echo 'No se ha sido encontrado el archivo';
+        exit();
+    }
     if(!empty($_POST)){
         
-        $marca_produc = mysqli_real_escape_string ($con, $_POST["marca_produc"]);
-        $nombre_produc = mysqli_real_escape_string ($con, $_POST["nombre_produc"]);
-        $talla = mysqli_real_escape_string ($con,$_POST["select_talla"]);
+        $marca_produc = mysqli_real_escape_string ($con, strip_tags($_POST["marca_produc"]));
+        $nombre_produc = mysqli_real_escape_string ($con, strip_tags($_POST["nombre_produc"]));
+        $talla = mysqli_real_escape_string ($con, strip_tags($_POST["select_talla"]));
         $imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
-        $precio = mysqli_real_escape_string ($con, $_POST["precio"]);
-        $color = mysqli_real_escape_string ($con, $_POST["select_color"]);
-        $stock = mysqli_real_escape_string ($con, $_POST["stock"]);
-        $categoria = mysqli_real_escape_string ($con,$_POST["select"]);
+        $precio = mysqli_real_escape_string ($con, strip_tags($_POST["precio"]));
+        $color = mysqli_real_escape_string ($con, strip_tags($_POST["select_color"]));
+        $stock = mysqli_real_escape_string ($con, strip_tags($_POST["stock"]));
+        $categoria = mysqli_real_escape_string ($con, strip_tags($_POST["select"]));
 
         $CrearproductoSql="INSERT INTO producto (marca, nombre, id_talla, precio, id_col, stock, id_cat, img, id_estado) VALUES
         ('".$marca_produc."','".$nombre_produc."','".$talla."','".$precio."','".$color."','".$stock."','".$categoria."','".$imagen."', 1);";
