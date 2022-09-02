110024580ccc589339cbe20f3c7ba3686621a031
Cristobal Diaz <pizzagames14@gmail.com>
2022-09-02 18:01:40 -0400

    revisar mensaje pendiente

--- modelos/ingresarProdBackend.php
+++ modelos/ingresarProdBackend.php
@@ -19,21 +19,21 @@
         $tam_imagen = $_FILES['imagen']['size'];
         $tip_imagen = $_FILES['imagen']['type'];
 
-        //validacion nombre repetido
+        //validacion datos imagen
+        if($tam_imagen>1000000 || $tip_imagen!="image/jpg" || $tip_imagen!="image/jpeg" || $tip_imagen!="image/png"){
+            echo '<script language="javascript">alert("archivo no permitido para subir"); window.location.href="../ingresarProducto.php"</script>';
+            exit();
+        }
+
+        $CrearproductoSql="INSERT INTO producto (marca, nombre, id_talla,id_gen ,precio , id_col, stock, id_cat, img) VALUES
+        ('".$marca_produc."','".$nombre_produc."','".$talla."','".$genero."','".$precio."','".$color."','".$stock."','".$categoria."','".$imagen."');";
         $ver_nombre = mysqli_query($con, "SELECT * from producto where nombre ='$nombre_produc'");
+        
         if(mysqli_num_rows($ver_nombre) > 0){
             echo '<script language="javascript">alert("Nombre ya ingresado, modifíquelo"); window.location.href="../ingresarProducto.php"</script>';
             exit();
         }
-
-        //validacion datos imagen
-        if($tam_imagen>1000000 || $tip_imagen!="image/jpg" || $tip_imagen!="image/jpeg" || $tip_imagen!="image/png"){
-            echo '<script language="javascript">alert("archivo no permitido para subir"); window.location.href="../ingresarProducto.php"</script>';
-            
-        }else{            
-        $CrearproductoSql="INSERT INTO producto (marca, nombre, id_talla,id_gen ,precio , id_col, stock, id_cat, img) VALUES
-        ('".$marca_produc."','".$nombre_produc."','".$talla."','".$genero."','".$precio."','".$color."','".$stock."','".$categoria."','".$imagen."');";
-
+    
         mysqli_query($con, $CrearproductoSql);
         
         unset($_POST["marca_produc"]);
@@ -47,7 +47,6 @@
         
         echo '<script language="javascript">alert("Datos Ingresados Correctamente"); window.location.href="../ingresarProducto.php"</script>';
 
-        }
     }
     
 ?>
