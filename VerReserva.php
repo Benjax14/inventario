78624ce3305c29b1a254b7edc44a48e7de413200
Sebastián Jerez Marín <104412736+SerapioUnlucky@users.noreply.github.com>
2022-09-04 05:08:20 -0300

    Update buscador

--- VerReserva.php
+++ VerReserva.php
@@ -17,7 +17,7 @@
 
                     <form action="./busquedaReservas.php" method="POST">
 
-                        <input type="text" name="busqueda" class="src" placeholder="Escriba aquí para buscar un cliente">
+                        <input type="search" name="busqueda" class="src" placeholder="Escriba aquí para buscar un cliente">
                 
                     </form>
 
